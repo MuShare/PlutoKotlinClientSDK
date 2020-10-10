df7a55f350d6a146f4516ae0e0b0d5bfe3927e83
Meng Li <lm2343635@126.com>
2020-10-11 01:00:44 +0900

    Upload avatar

--- demoapp/src/main/java/com/mushare/demoapp/ui/login/ProfileActivity.kt
+++ demoapp/src/main/java/com/mushare/demoapp/ui/login/ProfileActivity.kt
@@ -1,21 +1,14 @@
 package com.mushare.demoapp.ui.login;
 
 import android.app.Activity
-import android.graphics.Bitmap
-import android.graphics.BitmapFactory
 import android.os.Bundle
-import android.util.Base64
 import android.util.Log
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import com.github.dhaval2404.imagepicker.ImagePicker
 import com.mushare.demoapp.R
-import com.mushare.plutosdk.Pluto
-import com.mushare.plutosdk.getToken
-import com.mushare.plutosdk.myInfo
-import com.mushare.plutosdk.updateName
-import java.io.ByteArrayOutputStream
+import com.mushare.plutosdk.*
 import java.lang.ref.WeakReference
 
 class ProfileActivity : AppCompatActivity() {
@@ -35,12 +28,7 @@ class ProfileActivity : AppCompatActivity() {
         nameEditText = WeakReference(findViewById(R.id.profile_name))
         avatarImageView = WeakReference(findViewById(R.id.profile_avatar))
 
-        Pluto.getInstance()?.myInfo(success = { user ->
-            nameEditText.get()?.setText(user.name)
-            avatarImageView.get()?.let {
-                Glide.with(this).load(user.avatar).into(it)
-            }
-        })
+        updateUserInfo()
 
         Pluto.getInstance()?.getToken(completion = {
             findViewById<TextView>(R.id.profile_access_token).text = it ?: "Refresh failed"
@@ -52,17 +40,36 @@ class ProfileActivity : AppCompatActivity() {
                 .compress(64)
                 .maxResultSize(480, 480)
                 .start { resultCode, data ->
-                    if (resultCode == Activity.RESULT_OK) {
-                        val filePath = ImagePicker.getFilePath(data) ?: return@start
-                        val bitmap = BitmapFactory.decodeFile(filePath)
-                        val outputStream = ByteArrayOutputStream()
-                        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream)
-                        val base64 = Base64.encodeToString(outputStream.toByteArray(), Base64.DEFAULT)
-                        Log.d(TAG, "base64: $base64")
-                    } else if (resultCode == ImagePicker.RESULT_ERROR) {
-                        Toast.makeText(this, ImagePicker.getError(data), Toast.LENGTH_SHORT).show()
-                    } else {
-                        Toast.makeText(this, "Task Cancelled", Toast.LENGTH_SHORT).show()
+                    when (resultCode) {
+                        Activity.RESULT_OK -> {
+                            Pluto.getInstance()?.uploadAvatar(
+                                imageFile = ImagePicker.getFile(data) ?: return@start,
+                                success = {
+                                    updateUserInfo {
+                                        Toast
+                                            .makeText(
+                                                this,
+                                                "Avatar uploaded",
+                                                Toast.LENGTH_SHORT
+                                            )
+                                            .show()
+                                    }
+                                },
+                                error = {
+                                    Log.d(TAG, "Error uploading avatar $it")
+                                }
+                            )
+                        }
+                        ImagePicker.RESULT_ERROR -> {
+                            Toast
+                                .makeText(this, ImagePicker.getError(data), Toast.LENGTH_SHORT)
+                                .show()
+                        }
+                        else -> {
+                            Toast
+                                .makeText(this, "Task Cancelled", Toast.LENGTH_SHORT)
+                                .show()
+                        }
                     }
                 }
         }
@@ -72,9 +79,9 @@ class ProfileActivity : AppCompatActivity() {
             Pluto.getInstance()?.updateName(
                 name = name,
                 success = {
-                    Pluto.getInstance()?.myInfo(success = {
+                    updateUserInfo {
                         Toast.makeText(this, "Name updated", Toast.LENGTH_SHORT).show()
-                    })
+                    }
                 },
                 error = {
                     Log.d(TAG, "Error updating username $it")
@@ -87,5 +94,16 @@ class ProfileActivity : AppCompatActivity() {
                 findViewById<TextView>(R.id.profile_access_token).text = it ?: "Refresh failed"
             })
         }
+    }
+
+    private fun updateUserInfo(completion: (() -> Unit)? = null) {
+        Pluto.getInstance()?.myInfo(success = { user ->
+            nameEditText.get()?.setText(user.name)
+            avatarImageView.get()?.let {
+                Glide.with(this).load(user.avatar).into(it)
+            }
+            completion?.let { it() }
+        })
+
     }
 }
